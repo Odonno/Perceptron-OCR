8708eff4bf17dd530a4e6b735e72cc8329511255
Odonno <bottiau.david@laposte.net>
2014-04-03 20:47:55 +0200

    Add Perceptron Visual Studio C# Project

---
+++ Perceptron/Perceptron.OCR.UI/UserControls/RectanglePixel.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows.Controls;
+using Perceptron.OCR.Model.Model.Abstract;
+using Perceptron.OCR.ViewModel.ViewModel.Abstract;
+
+namespace Perceptron.OCR.UI.UserControls
+{
+    /// <summary>
+    /// Logique d'interaction pour RectanglePixel.xaml
+    /// </summary>
+    public partial class RectanglePixel : UserControl
+    {
+        #region Properties
+
+        public IPixel Pixel { get; private set; }
+
+        #endregion
+
+
+        #region Constructor
+
+        public RectanglePixel(IMainViewModel mainViewModel, IPixel pixel)
+        {
+            Pixel = pixel;
+            DataContext = mainViewModel;
+
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
