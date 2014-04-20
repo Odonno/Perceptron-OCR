d5283131950914d5139719aca7f7eb597e021f23
Odonno <bottiau.david@laposte.net>
2014-04-20 15:58:52 +0200

    Support switching pixel on MainView using OnMouseEnter event

--- Perceptron/Perceptron.OCR.UI/UserControls/RectanglePixel.xaml.cs
+++ Perceptron/Perceptron.OCR.UI/UserControls/RectanglePixel.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Controls;
+using System.Windows.Input;
 using Perceptron.OCR.Model.Model.Abstract;
 using Perceptron.OCR.ViewModel.ViewModel.Abstract;
 
@@ -9,6 +10,12 @@ namespace Perceptron.OCR.UI.UserControls
     /// </summary>
     public partial class RectanglePixel : UserControl
     {
+        #region Fields
+
+        private IMainViewModel _mainViewModel;
+
+        #endregion
+
         #region Properties
 
         public IPixel Pixel { get; private set; }
@@ -21,9 +28,27 @@ namespace Perceptron.OCR.UI.UserControls
         public RectanglePixel(IMainViewModel mainViewModel, IPixel pixel)
         {
             Pixel = pixel;
-            DataContext = mainViewModel;
+            _mainViewModel = mainViewModel;
+
+            DataContext = _mainViewModel;
 
             InitializeComponent();
+        }
+
+        #endregion
+
+
+        #region Events
+
+        private void Rectangle_OnMouseEnter(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                _mainViewModel.SwitchSelectionCommand.Execute(Pixel);
+        }
+
+        private void Rectangle_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _mainViewModel.SwitchSelectionCommand.Execute(Pixel);
         }
 
         #endregion
